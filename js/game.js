f85be662ec16d485aa5fd4a58e808b4504ccf50d
santosh mane <95619972+santoshmane101@users.noreply.github.com>
2021-12-18 13:41:59 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ function verify(){
         }
         else {
         alert("Please Login First...");
-        window.location.href = "https://santoshmane101.github.io/PROJECT-ROCK-PAPER-SCISSORS/index.html";
+        window.location.href = "index.html";
         }
     }
  
