84bddccaddfebc0ede7ca61bcb57489d1c47cb87
maheshd-idx <94886145+maheshd-idx@users.noreply.github.com>
2022-01-18 10:07:31 +0530

    Review comments

--- js/Login.js
+++ js/Login.js
@@ -1,12 +1,15 @@
  function enterName(){
      let name = prompt("Please Enter Your Name");
+  // Review - Add null check here
      sessionStorage.setItem("name", name)
  }
  function login(){
     let mail = document.getElementById('email').value;
     let pass = document.getElementById('pass').value;
-    if(mail=="sam@indexnine.com" && pass=="sam123"){
+   // Review - Use === instead of == 
+   if(mail=="sam@indexnine.com" && pass=="sam123"){
         window.location.href = "game.html";
+    // Review - Change the order of execution
         sessionStorage.setItem("email", "sam@indexnine.com");
         sessionStorage.setItem("pass", "sam123");
     }
